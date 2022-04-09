f3ea71862472c134929ae91c60455ecf6accae36
defunsm <salmanhossain500@gmail.com>
2022-04-09 15:22:11 -0400

    add chakra ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+
+// Setting up Chakara UI https://chakra-ui.com/guides/getting-started/nextjs-guide
+// TODO: Create a custom theme by extrending the theme provided by chakara ui
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+          <ChakraProvider>
+            <Component {...pageProps} />
+          </ChakraProvider>
+        )
 }
 
 export default MyApp
