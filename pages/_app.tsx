a1aa23b782ffe30478513a5900ede1f19873a9ac
defunsm <salmanhossain500@gmail.com>
2022-04-11 19:39:47 -0400

    adding fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import '../styles/index.css'
+import '../styles/index.scss'
 
 // Setting up Chakara UI https://chakra-ui.com/guides/getting-started/nextjs-guide
 // TODO: Create a custom theme by extrending the theme provided by chakara ui
