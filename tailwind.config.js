8c6bf1a00f7a19a156a60604b48296fc06b4cad1
defunsm <salmanhossain500@gmail.com>
2022-04-11 20:20:38 -0400

    add new styling for title

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./styles/**/*.{otf,ttf,woff2,scss}",
+    "./public/**/*.{otf,ttf,woff2,scss}"
   ],
   theme: {
     extend: {},
