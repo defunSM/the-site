0c25f538af3be9b736fc00375c3e095a40fc8984
defunsm <salmanhossain500@gmail.com>
2022-05-08 21:54:16 -0400

    add: Title, Footer component

---
+++ components/Title.tsx
@@ -0,0 +1,13 @@
+import styles from '../styles/Home.module.css'
+
+function Title(){
+    return (
+        <>
+          <h1 className={styles.title}>
+            Salman // <span className="rainbow-text alias">defunSM</span>
+          </h1>
+        </>
+    )
+}
+
+export default Title
